ace88a608a56e24cfd2267b8d9e2d1be5360797a
melissajimison <melissajimison@gmail.com>
2016-04-30 18:07:36 -0700

    v

--- scrabble_sin.rb
+++ scrabble_sin.rb
@@ -5,7 +5,7 @@ require_relative 'libs/points'
 class Scrabble < Sinatra::Base
 
   get '/' do
-    send_file File.join(settings.public_folder, 'index.html')
+    send_file File.join(settings.public_folder, 'index.erb')
     erb :index
 end
 
